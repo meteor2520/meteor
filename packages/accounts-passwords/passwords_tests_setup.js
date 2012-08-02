8db73ec331509cc43f496f63e7089afd317ff1b8
Nick Martin <nim@meteor.com>
2012-08-01 17:34:10 -0700

    Not much testing we can do on new Meteor.accounts.config stuff without investing a bunch of work.

--- packages/accounts-passwords/passwords_tests_setup.js
+++ packages/accounts-passwords/passwords_tests_setup.js
@@ -20,3 +20,23 @@ Meteor.methods({
     }
   }
 });
+
+
+// Because this is global state that affects every client, we can't turn
+// it on and off during the tests. Doing so would mean two simultaneous
+// test runs could collide with each other.
+//
+// We should probably have some sort of server-isolation between
+// multiple test runs. Perhaps a separate server instance per run. This
+// problem isn't unique to this test, there are other places in the code
+// where we do various hacky things to work around the lack of
+// server-side isolation.
+//
+// For now, we just test the one configuration state. You can comment
+// out each configuration option and see that the tests fail.
+Meteor.accounts.config({
+  unsafePasswordChanges: true,
+  // accounts - updateOrCreateUser can't work w/ these true.
+  requireEmail: false,
+  requireUsername: false
+});
